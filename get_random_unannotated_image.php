eeda93806dc7628683514495c0895c1abcd97066
n <norman.koch@tu-dresden.de>
2026-05-13 10:21:31 +0200

    fix

--- get_random_unannotated_image.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include_once("functions.php");
-
-	if(get_get("like")) {
-		print get_next_random_unannotated_image(get_get("like"));
-	} else {
-		print get_next_random_unannotated_image();
-	}
-?>
