79bcc4d1b31e98aa5578262bcacaf5eb5df5cf37
Norman Koch <norman.koch@tu-dresden.de>
2026-06-04 10:16:02 +0200

    fix

--- docker.sh
+++ docker.sh
@@ -135,7 +135,7 @@ networks:
 EOL
 
 if ! docker compose &>/dev/null; then
-    sudo apt-get install -y docker-compose-plugin || sudo apt-get install -y docker-compose-v2
+    sudo apt-get install -y docker-compose
 fi
 
 CMD="docker compose"
